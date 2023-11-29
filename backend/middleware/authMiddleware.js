86df693c70f35dce33b9bb6a8c625cd999dc1380
Kledis <kledis.myrtollari1@gmail.com>
2023-11-29 20:05:42 +0100

    cleaning some console.logs

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -10,18 +10,15 @@ const protect = asyncHandler(async (req, res, next) => {
       !req.headers.authorization ||
       !req.headers.authorization.startsWith("Bearer")
     ) {
-      console.log("No token present in headers");
       res.status(401);
       throw new Error("Not authorized, no token");
     }
 
     // Get token from header
     token = req.headers.authorization.split(" ")[1];
-    console.log("Token in Headers:", req.headers.authorization);
 
     // Verify token
     const decoded = jwt.verify(token, "secret");
-    console.log("Decoded Token:", decoded);
 
     // Get admin from the token
     req.admin = await Admin.findById(decoded.id).select("-password");

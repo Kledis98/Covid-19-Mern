ef098ff2dacdd1ea741038f2f2162e56e7539930
Kledis <kledis.myrtollari1@gmail.com>
2023-12-01 00:26:22 +0100

    testing

--- frontend/src/components/Table/Table.jsx
+++ frontend/src/components/Table/Table.jsx
@@ -3,28 +3,26 @@ import axios from "axios";
 import "./Table.css";
 import numeral from "numeral";
 
+
 function CountryTable() {
   const [countriesData, setCountriesData] = useState([]);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          "https://covid-19-tracker-mt79.onrender.com/countries?date=2023-03-08"
+          "https://covid-19-tracker-mt79.onrender.com/flagsdata"
         );
 
         if (response.status === 200) {
           const sortedCountriesData = response.data.sort(
-            (a, b) => b.totalCases - a.totalCases
+            (a, b) => b.cases - a.cases
           );
     
           setCountriesData(sortedCountriesData);
         }
       } catch (error) {
         console.error("Error fetching data:", error);
-      } finally {
-        setLoading(false);
       }
     };
 
@@ -32,24 +30,19 @@ function CountryTable() {
   }, []);
 
   return (
-    <div className="table">
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        countriesData.map((country) => (
-          <tr key={country._id}>
-            <td>{country.name}</td>
-            <td>
-              {country.totalCases !== undefined ? (
-                <strong>{numeral(country.totalCases).format("0,0")}</strong>
-              ) : (
-                "N/A"
-              )}
-            </td>
-          </tr>
-        ))
-      )}
-    </div>
+    
+      <div className="table">
+       
+          {countriesData.map((country) => (
+            <tr key={country._id}>
+              <td>{country.name}</td>
+              <td>
+              <strong>{numeral(country.cases).format("0,0")}</strong>
+              </td>
+            </tr>
+          ))}
+      </div>
+    
   );
 }
 

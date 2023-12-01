d09eba38394050ee4814965cec90823d2afef7ab
Kledis <kledis.myrtollari1@gmail.com>
2023-12-01 02:36:03 +0100

    New CY test added

---
+++ frontend/cypress/e2e/1-getting-started/testingAdmin.cy.js
@@ -0,0 +1,13 @@
+describe("Login Test", () => {
+  it("should log in with username and password", () => {
+    cy.visit("http://localhost:3000/login");
+
+    cy.get('[data-test="username-input"]').type("kledis123");
+
+    cy.get('[data-test="password-input"]').type("kledis123");
+
+    cy.get('[data-test="login-button"]').click();
+
+    cy.url().should("eq", "http://localhost:3000/admin-panel");
+  });
+});

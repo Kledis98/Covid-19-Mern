a0434c3f9603d16d6cd76c4e022806dc6d95f601
Kledis <kledis.myrtollari1@gmail.com>
2023-12-01 02:24:31 +0100

    cypress tests

--- frontend/cypress/e2e/1-getting-started/fetchData.cy.js
+++ frontend/cypress/e2e/1-getting-started/fetchData.cy.js
@@ -1,31 +1,16 @@
 // cypress/integration/your_test_spec.js
 
 describe("Your Test Suite", () => {
-  it("should display 6 infoboxes after selecting country, date, and clicking fetch data", () => {
+  it("should display the selected country in the dropdown", () => {
     // Visit your application
     cy.visit("http://localhost:3000/");
 
     // Interact with the dropdown to select a country
-    cy.get('[data-test="country-dropdown"]').select(
-      "6560d09c1250b04f8e66231f",
-      {
-        force: true,
-      }
-    );
+    cy.get('[data-test="country-dropdown"]').select("6568a39f8146de17edea75de");
 
-    // Interact with the date input to select a date
-    cy.get('[data-test="date-input"]').type("2023-01-01");
-
-    // Interact with the fetch data button
-    cy.get('[data-test="fetch-button"]').click();
-
-    // Assert that the 6 infoboxes are visible
-    cy.get('[data-test="grid-container"]').should("exist").should("be.visible");
-    cy.get('[data-test="grid-container"] [data-test="grid-first-row"]').should(
-      "exist"
-    );
-    cy.get('[data-test="grid-container"] [data-test="grid-second-row"]').should(
-      "exist"
-    );
+    // Assert the selected option text is "Albania"
+    cy.get('[data-test="country-dropdown"]')
+      .find(":selected")
+      .should("have.text", "Albania");
   });
 });
--- frontend/cypress/e2e/1-getting-started/login-test.cy.js
+++ frontend/cypress/e2e/1-getting-started/login-test.cy.js
@@ -3,7 +3,7 @@
 describe("Login Button", () => {
   it("should navigate to /login when the login button is clicked", () => {
     // Visit the page containing the login button
-    cy.visit("http://localhost:3000"); // Update the URL as needed
+    cy.visit("http://localhost:3000/"); // Update the URL as needed
 
     // Click the login button
     cy.get('[data-test = "login-button"]').click();

95b9850b029d7c378e6c6d0e3784b73b3a34c48f
Kledis <kledis.myrtollari1@gmail.com>
2023-12-01 10:17:42 +0100

    cypress tests

--- frontend/cypress/e2e/1-getting-started/fetchData.cy.js
+++ frontend/cypress/e2e/1-getting-started/fetchData.cy.js
@@ -2,13 +2,10 @@
 
 describe("Your Test Suite", () => {
   it("should display the selected country in the dropdown", () => {
-    // Visit your application
-    cy.visit("http://localhost:3000/");
+    cy.visit("http://localhost:3000");
 
-    // Interact with the dropdown to select a country
     cy.get('[data-test="country-dropdown"]').select("6568a39f8146de17edea75de");
 
-    // Assert the selected option text is "Albania"
     cy.get('[data-test="country-dropdown"]')
       .find(":selected")
       .should("have.text", "Albania");

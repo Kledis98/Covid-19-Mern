3ab58a864a7004c5828929db38a444427f81e32d
Kledis <kledis.myrtollari1@gmail.com>
2023-12-01 10:02:25 +0100

    Cypress env

--- frontend/cypress/e2e/1-getting-started/testingAdmin.cy.js
+++ frontend/cypress/e2e/1-getting-started/testingAdmin.cy.js
@@ -2,9 +2,13 @@ describe("Login Test", () => {
   it("should log in with username and password", () => {
     cy.visit("http://localhost:3000/login");
 
-    cy.get('[data-test="username-input"]').type("kledis123");
+    cy.get('[data-test="username-input"]').type(
+      Cypress.env("CYPRESS_USERNAME")
+    );
 
-    cy.get('[data-test="password-input"]').type("kledis123");
+    cy.get('[data-test="password-input"]').type(
+      Cypress.env("CYPRESS_PASSWORD")
+    );
 
     cy.get('[data-test="login-admin-button"]').click();
 
